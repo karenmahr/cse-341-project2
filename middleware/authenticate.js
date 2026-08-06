d48ed47710d72cdef836aff4ec483f5175a860a6
karenmahr <kmahr@byupathway.edu>
2026-08-06 19:55:13 -0300

    changed the authentication function

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,14 +1,10 @@
 const isAuthenticated = (req, res, next) => {
-    if (req.session.user !== undefined) {
-        return next();
+    if (req.session.user === undefined) {
+        return res.status(401).json("You do not have access.");
     }
+    next();
+}
 
-    if (req.headers.referer && req.headers.referer.includes('/api-docs')) {
-        return next();
-    }
-
-    return res.status(401).json("You do not have access.");
-};
 module.exports = {
     isAuthenticated
 }
