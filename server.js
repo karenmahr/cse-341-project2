5896b5011a09dd87345d4d235d4eeacbc6269e29
karenmahr <kmahr@byupathway.edu>
2026-08-06 20:14:40 -0300

    updated mi cookie config

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app
         saveUninitialized: true,
         cookie: {
             sameSite: 'none',
-            secure: true,
+            secure: auto,
             httpOnly: true
         }
     }))
